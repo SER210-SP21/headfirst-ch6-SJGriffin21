2156ef0297ed5dffa41e01ffdad13fefb88e4987
SJGriffin21 <sjgriffin@quinnipiac.edu>
2023-02-12 14:11:03 -0500

    Initial commit

---
+++ app/src/main/java/edu/quinnipiac/ser210/chapter6demo/WelcomeFragment.kt
@@ -0,0 +1,36 @@
+package edu.quinnipiac.ser210.chapter6demo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [WelcomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class WelcomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_welcome, container, false)
+        val startButton = view.findViewById<Button>(R.id.start)
+
+        startButton.setOnClickListener {
+            view.findNavController().navigate(R.id.action_welcomeFragment_to_messageFragment)
+        }
+        return view
+    }
+
+}
